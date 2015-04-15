a06700ff92dc0e2f8680abf83aa9504c777b0205
Travis <travis@mcdem.us>
2015-04-14 22:31:21 -0400

    Translate structs to d3 compatible JSON

--- data/transformers/unmarshalxml.go
+++ data/transformers/unmarshalxml.go
@@ -1,35 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"flag"
-	"fmt"
+
 	"io/ioutil"
 	"log"
 	"os/exec"
+	"strconv"
 )
 
-var inputFilename string
-var defaultPath = "en.atm.co2e.kt_Indicator_en_xml_v2.xml"
-
 func main() {
-	flag.StringVar(&inputFilename, "in",
-		defaultPath, "The XML file of data to unmarshal")
+	args := struct {
+		inFile    string
+		outFile   string
+		debugFile string
+	}{}
+	flag.StringVar(&args.inFile, "in",
+		"en.atm.co2e.kt_Indicator_en_xml_v2.xml",
+		"The XML file of data to unmarshal")
+	flag.StringVar(&args.debugFile, "debug",
+		"/tmp/debug.xml",
+		"The file to output marshalled JSON to.")
+	flag.StringVar(&args.outFile, "out",
+		"co2e.json",
+		"A file for debugging intermediary state.")
 	flag.Parse()
-	in, err := decruft(inputFilename)
-	ioutil.WriteFile("wat.xml", in, 0644)
+
+	in, err := decruft(args.inFile)
+	ioutil.WriteFile(args.outFile, in, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	var r Root
 	err = xml.Unmarshal(in, &r)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", r)
+	err = toJSON(r.Records, args.outFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-type Root struct {
+// Types for unmarshalling XML.
+type Root struct { // ???(aoeu): Why is this struct needed?
 	Records []Record `xml:"Data>Record"`
 }
 
@@ -37,6 +54,41 @@ type Record struct {
 	Country string
 	Year    int
 	Value   float64
+}
+
+// Types for marshalling JSON.
+type Countries []Country
+type Country map[string]float64
+
+func toJSON(rr []Record, outFile string) error {
+	// Country -> Year -> C02 emission value
+	t := make(map[string]map[string]float64)
+	for _, r := range rr {
+		if _, ok := t[r.Country]; !ok {
+			t[r.Country] = make(map[string]float64)
+		}
+		t[r.Country][strconv.Itoa(r.Year)] = r.Value
+	}
+	// The data is arranged as a series of JSON objects.
+	// This is either to appease d3 or some intermediary javascript.
+	j := make([]map[string]interface{}, 0)
+	for c, y := range t {
+		e := make(map[string]interface{})
+		e["Country"] = c
+		for k, v := range y {
+			e[k] = v
+		}
+		j = append(j, e)
+	}
+	b, err := json.MarshalIndent(j, "", "	")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(outFile, b, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func decruft(filename string) ([]byte, error) {
